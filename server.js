493514d2fcf8e27038ca60f9b1c79268e2a8c7c7
Florian Töns <floriantoens273@gmail.com>
2025-05-07 17:02:27 +0000

    Edit server.js

--- server.js
+++ server.js
@@ -20,9 +20,8 @@ const mongoCollectionName = 'products';
 app.use(cors());
 app.use(express.json());
 // In server.js, nach app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(express.limit(100000000));
 
 let productsCollection;
