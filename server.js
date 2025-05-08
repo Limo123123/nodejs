9d50680cc87c614845a18fa827ff48a8139dab62
Florian Töns <floriantoens273@gmail.com>
2025-05-08 18:52:37 +0000

    Edit server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(session({
     secret: sessionSecret,
-    resave: false, saveUninitialized: false,
+    resave: true, saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: mongoUri, dbName: mongoDbName, collectionName: 'sessions', ttl: 14 * 24 * 60 * 60 }),
     cookie: {
     secure: true, // Muss true sein für SameSite=None
