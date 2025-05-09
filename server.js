125030292bb820da7cf71382a47c3f6d661719e3
Florian Töns <floriantoens273@gmail.com>
2025-05-09 13:16:28 +0000

    Edit server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.set('trust proxy', 1); // WICHTIG für secure cookies hinter Proxy
 
 const HTTP_PORT = process.env.PORT || 80;
-const SEED_PRODUCTS_FILE = 'products_seed.json';
+const SEED_PRODUCTS_FILE = 'products.json';
 const TIMEZONE = 'Europe/Berlin';
 
 // --- Konfiguration aus Umgebungsvariablen ---
