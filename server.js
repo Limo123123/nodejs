2d2ef677207dc39c07d904e128cce418326c95df
Florian Töns <floriantoens273@gmail.com>
2025-06-21 22:43:48 +0000

    Edit server.js

--- server.js
+++ server.js
@@ -96,7 +96,8 @@ app.use(session({
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
         maxAge: 14 * 24 * 60 * 60 * 1000,
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+		partitioned: process.env.NODE_ENV === 'production'
     }
 }));
 
