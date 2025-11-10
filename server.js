f5e2a92333d639f289e848b42ecc8483e2d8db17
Florian Töns <floriantoens273@gmail.com>
2025-11-10 19:14:54 +0000

    Hating CORS even more

--- server.js
+++ server.js
@@ -78,17 +78,7 @@ const allowedOrigins = [frontendDevUrlHttp, frontendDevUrlHttps];
 if (frontendProdUrl) { allowedOrigins.push(frontendProdUrl); }
 console.log(`${LOG_PREFIX_SERVER} Erlaubte CORS Origins:`, allowedOrigins);
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            console.error(`${LOG_PREFIX_SERVER} CORS Fehler: Origin ${origin} nicht erlaubt.`);
-            callback(new Error(`Origin ${origin} nicht durch CORS erlaubt`));
-        }
-    },
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(session({
