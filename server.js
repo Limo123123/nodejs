ff9fbfc03b8eca3970adc5c20db09725d99dbc49
Florian Töns <floriantoens273@gmail.com>
2025-12-09 16:02:06 +0000

    Edit server.js

--- server.js
+++ server.js
@@ -3321,8 +3321,6 @@ async function seedSubjects() {
         console.log(`${LOG_PREFIX_SERVER} Standardfächer für Anne Frank Realschule angelegt.`);
     }
 }
-// Einmalig beim Start ausführen
-seedSubjects().catch(console.error);
 
 // Hilfsfunktion: Durchschnitt dynamisch berechnen
 async function updateTeacherAverage(teacherId) {
