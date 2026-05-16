c1959974ceffed8ec47c8562feb27a8d081a2c1f
Florian Töns <limo64794@gmail.com>
2026-05-16 22:23:56 +0000

    Smaller max input

--- server.js
+++ server.js
@@ -112,8 +112,8 @@ app.use(cors({
     },
     credentials: true
 }));
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true, parameterLimit: 50000 }));
 app.use(session({
     secret: sessionSecret,
     resave: false,
